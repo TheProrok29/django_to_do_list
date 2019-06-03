fdb4b31cc4d0b2bf2cbe2a390a55ac1b1380d05b
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-06-03 17:04:28 +0200

    Rewrite functional tests to using unittest module
    Delete pkg-resources from requirements.txt

---
+++ to_do_list/functional_tests.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def terDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_latter(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
