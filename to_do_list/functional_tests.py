fddf0ba0801a6bb986f3b919387f44f30c86c06b
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-06-05 14:55:19 +0200

    Front page HTML now generated from a template

--- to_do_list/functional_tests.py
+++ to_do_list/functional_tests.py
@@ -31,7 +31,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1 : Learn Python' for row in rows)
+            any(row.text == '1 : Learn Python' for row in rows),
+            "New to-to item did not appear in table"
         )
 
         self.fail('Finish the test!')
