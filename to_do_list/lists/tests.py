0003e5f71caaa921e2f867477ecd374cb30f2eb5
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-06-06 14:05:07 +0200

    Model for list Items and associated migration

--- to_do_list/lists/tests.py
+++ to_do_list/lists/tests.py
@@ -1,8 +1,5 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -15,8 +12,27 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second item')
