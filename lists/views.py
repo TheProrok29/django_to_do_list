7493fb6f71bd497564b60b12ba52e6576fa305a0
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-06-12 11:48:31 +0200

    Reorganize project structure

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 
 from lists.models import Item, List
 
 
 def home_page(request):
     return render(request, 'home.html')
-
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
@@ -22,3 +17,8 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
--- readme.md
+++
@@ -1,6 +0,0 @@
-[![Build Status](https://travis-ci.org/TheProrok29/django_to_do_list.svg?branch=master)](https://travis-ci.org/TheProrok29/django_to_do_list)
-
-
-# To-Do Django app
-
-ToDo list created using Django framework with TDD metodology.
