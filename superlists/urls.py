a2bb770e1b571f5b13a8727549f21d38a7708bd7
Tomasz <tomaszdbogacki@gmail.com>
2019-07-08 21:30:43 +0200

    Start a new app accounts
    * configure basic routing for new app
    * add navbar to base.html

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,5 +5,6 @@ from lists import urls as list_urls
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(accounts_urls)),
 ]
 
