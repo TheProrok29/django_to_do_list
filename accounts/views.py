54a18ccd9d597bf7f0b929aa6ff5186e553ddb36
Tomasz <tomaszdbogacki@gmail.com>
2019-07-09 18:39:52 +0200

    Create a view to create a token associated to user email address. FT still broken.
    * Create a template for email message
    * Add basic email config in settings.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
+import uuid
+import sys
 from django.shortcuts import render
+from django.core.mail import send_mail
+from accounts.models import Token
 
-# Create your views here.
+
+def send_login_email(request):
+    email = request.POST['email']
+    uid = str(uuid.uuid4())
+    Token.objects.create(email=email, uid=uid)
+    print('saving uid', uid, 'for email', email, file=sys.stderr)
+    url = request.build_absolute_uri(f'/accounts/login?uid={uid}')
+    send_mail(
+        'Your login link for Superlists',
+        f'Use this link to log in:\n\n{url}',
+        'noreply@superlists',
+        [email],
+    )
+    return render(request, 'login_email_sent.html')
