59024439ee792344260c30f6e04a9c2009a073ea
Tomasz <tomaszdbogacki@gmail.com>
2019-07-10 19:09:06 +0200

    Add basic routing in accounts app
    * Create basic view in accounts app to test redirect

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class SendLoginEmailViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        })
+        self.assertRedirects(response, '/')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def send_login_email(request):
+    return redirect('/')
