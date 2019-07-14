002024e4bf54407c9fdcfb9469ff7c7d615a9d4f
Tomasz <tomaszdbogacki@gmail.com>
2019-07-14 22:08:42 +0200

    Add success message to accounts/views and base.html

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,4 +1,8 @@
+from unittest.mock import call
+from unittest.mock import patch
+
 from django.test import TestCase
+
 import accounts.views
 
 
@@ -30,3 +34,45 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(self.subject, 'Your login link for Superlists')
         self.assertEqual(self.from_email, 'kontaktletsdoittom@gmail.com')
         self.assertEqual(self.to_list, ['prorok29@vp.pl'])
+
+    @patch('accounts.views.send_mail')
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        })
+
+        self.assertEqual(mock_send_mail.called, True)
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'kontaktletsdoittom@gmail.com')
+        self.assertEqual(to_list, ['prorok29@vp.pl'])
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, "success")
+
+    @patch('accounts.views.messages')
+    def test_adds_success_message_with_mocks(self, mock_messages):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        })
+
+        expected = "Check your email, we've sent you a link you can use to log in."
+        self.assertEqual(
+            mock_messages.success.call_args,
+            call(response.wsgi_request, expected),
+        )
+
+
+class LoginViewTest(TestCase):
+    def test_redirects_to_home_page(self):
+        respone = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(respone, '/')
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def send_login_email(request):
     email = request.POST['email']
+    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
-        'body text abc',
+        'Use this link to log in',
         'kontaktletsdoittom@gmail.com',
         [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
