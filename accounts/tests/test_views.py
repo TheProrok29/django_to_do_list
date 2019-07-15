d19cd30dfd32aaa79361fe0f0a3cc5072573b612
Tomasz <tomaszdbogacki@gmail.com>
2019-07-15 14:03:52 +0200

    Add token to email. FT broken, UT pass
    * Create routing for login

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 from django.test import TestCase
 
 import accounts.views
+from accounts.models import Token
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -76,3 +77,20 @@ class LoginViewTest(TestCase):
     def test_redirects_to_home_page(self):
         respone = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(respone, '/')
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'prorok29@vp.pl')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'prorok29@vp.pl'
+        })
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
-from django.core.mail import send_mail
-from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+
+from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in',
+        message_body,
         'kontaktletsdoittom@gmail.com',
         [email],
     )
@@ -16,4 +23,8 @@ def send_login_email(request):
         request,
         "Check your email, we've sent you a link you can use to log in."
     )
+    return redirect('/')
+
+
+def login(request):
     return redirect('/')
