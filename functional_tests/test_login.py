dd313de3d13ee69c908acbf62ec342b882db81e9
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-09-27 12:27:45 +0200

    Adapting FT to be able to test real emails via POP3

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,20 +1,60 @@
+import os
+import poplib
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
 import re
 from .base import FunctionalTest
 
-TEST_EMAIL = 'prorok29@vp.pl'
+
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop3.poczta.onet.pl')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['ONET_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = 1
+                        body = '\n'.join(lines)
+                        return body
+                    time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_log_in(self):
         # Tom goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling him to enter her email address, so he does
+
+        if self.staging_server:
+            test_email = 'prorok29@vp.pl'
+        else:
+            test_email = 'prorok29@vp.pl'
+        
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent
@@ -24,15 +64,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # He checks his email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -40,10 +78,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # He is logged in !
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # Now he logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # He is logged out
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
